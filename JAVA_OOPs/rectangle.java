e3abf4c44e0b54d9a7cfc63b4cec1a2b342c034a
anandu-pn <anandupn@outlook.com>
2025-02-20 14:51:11 +0530

    Rectangle and circle

---
+++ JAVA_OOPs/rectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class rectangle{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your option for find area 1:rectangcle 2:circle");
+        System.out.print("Enter the length of rectangle: ");
+        double length=scanner.nextDouble();
+        System.out.print("Enter the width of rectangle: ");
+        double width=scanner.nextDouble();
+        double area = length*width;
+        System.out.println("The area is: " +area);
+    }
+}
